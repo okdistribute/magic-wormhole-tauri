eaead08509c522fb6c2dda94f4661a8471f171ee
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-03-30 11:59:38 -0700

    create noise pair

--- src-tauri/src/crypto.rs
+++ src-tauri/src/crypto.rs
@@ -2,51 +2,12 @@ use snow::{
     Builder,
 };
 
-fn get_inc_key(start: u8) -> [u8; 32] {
-    let mut k = [0u8; 32];
-    for i in 0..32 {
-        k[i] = start + i as u8;
-    }
-    k
-}
 
-fn create_keypair () {
+fn create_noise (psk: &[u8]) {
     let params: NoiseParams = "Noise_XXpsk3_25519_ChaChaPoly_SHA256".parse().unwrap();
-    return Builder::new(params.clone())
-            .local_private_key(&get_inc_key(0))
-            .build_initiator()
-            .unwrap();
+    let builder = Builder::new(params.clone());
+    let static_key = builder.generate.keypair().unwrap();
+    let mut noise = builder.local_private_key(&static_key.private).psk(3, psk).build_responder().unwrap();
+    return noise;
 }
 
-
-fn step_1 (key) {
-    // XX(s, rs):
-    // -> e
-    let len = key.write_message(&[], &mut buf).unwrap();
-    //let _ = h_r.read_message(&buf[..len], &mut buf2).unwrap();
-}
-
-fn step_2 (key) {
-    // <- e, ee s, es
-    let len = key.write_message(&[], &mut buf).unwrap();
-    //let _ = h_i.read_message(&buf[..len], &mut buf2).unwrap();
-}
-
-fn step_3 (key, psk: [u8; 32]) {
-    key.set_psk(3, &psk).unwrap();
-}
-
-fn test_set_psk() {
-    let mut h_r =
-        Builder::new(params).local_private_key(&get_inc_key(1)).build_responder().unwrap();
-
-    let mut buf = [0u8; 1024];
-    let mut buf2 = [0u8; 1024];
-
-    let psk = get_inc_key(3);
-
-
-    // -> s, se, psk
-    let len = h_i.write_message(&[], &mut buf).unwrap();
-    let _ = h_r.read_message(&buf[..len], &mut buf2).unwrap();
-}
